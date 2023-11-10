eb08d7bc2d30431f5c454cca065ee81493314cf2
Robert Mazan <mazan.robert@gmail.com>
2023-11-10 22:02:13 +0100

    Taking into account context path on GitHub Pages

--- packages/workspace-starter/vite.config.ts
+++ packages/workspace-starter/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/epam-openfin-demo/workspace-starter',
     build: {
         outDir: '../../docs/workspace-starter',
         emptyOutDir: true
